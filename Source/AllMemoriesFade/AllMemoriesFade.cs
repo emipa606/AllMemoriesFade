8e6aafba5c6321bfe46b773514c7ff6213c42852
Mlie <emipa606@users.noreply.github.com>
2022-04-26 22:27:22 +0200

    First publish

---
+++ Source/AllMemoriesFade/AllMemoriesFade.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace AllMemoriesFade;
+
+[StaticConstructorOnStartup]
+public class AllMemoriesFade
+{
+    static AllMemoriesFade()
+    {
+        foreach (var thoughtDef in DefDatabase<ThoughtDef>.AllDefs.Where(def => def.durationDays > 0))
+        {
+            thoughtDef.lerpMoodToZero = true;
+        }
+    }
+}
